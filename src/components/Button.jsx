6efad740a13ccd047f507014c250c819248acbcd
abdallah <abdallahelmekawy6@gmail.com>
2024-01-01 18:35:02 +0200

    subscribe section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ const Button = ({ label, iconUrl, customClass }) => {
     <div
       className={`flex justify-center items-center 
       gap-3 py-4 border border-orange-700 px-8 leading-none
-    text-white text-lg bg-orange-600 self-start rounded-full font-montserrat ${customClass}`}
+    text-white text-lg bg-orange-600 self-start rounded-full cursor-pointer font-montserrat ${customClass}`}
     >
       {label} {iconUrl && <img src={iconUrl} alt="icon" />}
     </div>
--- src/sections/Subscribe.jsx
+++ src/sections/Subscribe.jsx
@@ -1,5 +1,22 @@
+import Button from "../components/Button";
+
 const Subscribe = () => {
-  return <div>Subscribe</div>;
+  return (
+    <div className="max-w-[1440px] mx-auto grid sm:grid-cols-[repeat(auto-fit,minmax(30rem,1fr))] gap-y-8 gap-x-14 justify-center items-center">
+      <h1 className="text-4xl sm:text-5xl font-bold font-palanquin max-lg:text-center leading-normal">
+        Sign Up for<span className="text-orange-600"> Updates </span>&
+        Newsletter
+      </h1>
+      <div className="w-full border p-2 pl-4 items-center border-gray-700 rounded-full flex">
+        <input
+          className="w-full h-full rounded-full outline-none"
+          type="text"
+          placeholder="subscribe@nike.com"
+        />
+        <Button label="Sign up" customClass="whitespace-nowrap" />
+      </div>
+    </div>
+  );
 };
 
 export default Subscribe;
